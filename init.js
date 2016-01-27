1475bc0214ee5ea61bc2afbda5643a177bac8fde
tvaz <tvaz@pugetsound.edu>
2016-01-27 15:24:04 -0800

    raphael stuff

--- init.js
+++ init.js
@@ -8,9 +8,16 @@ function init(){
   paper = new Raphael(8, 8, canvas.width, canvas.height);
   var circle = paper.circle(50,40,10);
   circle.attr("fill","#f00");
+
+/*
+var lines = paper.set();
+for (line in poem){
+	lines.push(paper.text(330, 100, line);
 }
+	lines.attr({font: "12px Fontin-Sans, Arial", fill: "#000", "text-anchor": "start"});
+*/
 
-
+}
 $(document).ready(function() {
     init();
 });
